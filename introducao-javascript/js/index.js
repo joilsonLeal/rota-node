bcd8097b64e1323358d89ad9649187229187fc5b
joilson <joilson_rg@hotmail.com>
2020-09-24 09:40:38 -0300

    feature: faz validação dos campos altura e peso

--- introducao-javascript/js/index.js
+++ introducao-javascript/js/index.js
@@ -3,10 +3,27 @@ let pacientes = document.querySelectorAll('.paciente');
 pacientes.forEach(paciente => {
     let peso = paciente.querySelector('.info-peso').textContent;
     let altura = paciente.querySelector('.info-altura').textContent;
-    let imc = paciente.querySelector('.info-imc');
+    let imcTd = paciente.querySelector('.info-imc');
     
-    imc.textContent = calcularIMC(peso, altura);
+    if(!validarPeso(peso))
+        imcTd.textContent = 'Peso inválido!';
+    else if (!validarAltura(altura))
+        imcTd.textContent = 'Altura inválida!';
+    else
+        imcTd.textContent = calcularIMC(peso, altura);
 });
+
+function validarPeso(peso) {
+    if(peso < 0  || peso >= 600)
+        return false;
+    return true;
+}
+
+function validarAltura(altura) {
+    if(altura < 0  || altura >= 3.0)
+        return false;
+    return true;
+}
 
 function calcularIMC(peso, altura) {
     return (parseFloat(peso) / (parseFloat(altura) * 2)).toFixed(1);
