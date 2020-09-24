819a994331d45a17d816542545a5cb8e46540fb8
joilson <joilson_rg@hotmail.com>
2020-09-24 16:37:52 -0300

    chore: altera nome da variável mensagemErro para buscaErro

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -1,16 +1,16 @@
 let buscarPacientes = document.querySelector('#buscar-pacientes');
-let mensagemErro = document.querySelector('#busca-erro');
+let buscaErro = document.querySelector('#busca-erro');
 
 
 buscarPacientes.addEventListener('click', async () => {
     let pacientes = await fetch('http://api-pacientes.herokuapp.com/pacientes')
         .then(response => response.json())
         .catch(err => {
-            mensagemErro.textContent = 'Não foi possivel carregar os pacientes.'
+            buscaErro.textContent = 'Não foi possivel carregar os pacientes.'
         });
     
     if(pacientes) {
         pacientes.forEach(paciente => adicionarPaciente(paciente));
-        mensagemErro.textContent = '';
+        buscaErro.textContent = '';
     }
 });
