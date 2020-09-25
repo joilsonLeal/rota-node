ede98462a764bb3148969bcda712c798364d3d1e
joilson <joilson_rg@hotmail.com>
2020-09-25 13:16:47 -0300

    feature: adiciona negociacaoController

---
+++ ES6-parte-1/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,24 @@
+class NegociacaoController {
+
+    constructor() {
+        const $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let data = new Date(this._inputData.value.split('-'));
+        
+        const negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+    }
+}
