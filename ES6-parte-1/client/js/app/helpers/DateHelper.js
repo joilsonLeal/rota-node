da56f022cc885e2dd3add04648cb15698461f1a4
joilson <joilson_rg@hotmail.com>
2020-09-25 14:41:48 -0300

    chore: adiciona validação de texto e lança erro ao instanciar DateHelper

--- ES6-parte-1/client/js/app/helpers/DateHelper.js
+++ ES6-parte-1/client/js/app/helpers/DateHelper.js
@@ -1,12 +1,19 @@
 class DateHelper {
-    static textoParaData(texto) {
-        return new Date(...
-            texto.split('-')
-            .map((item, index) => item - index % 2)
-        );
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada.');
     }
 
     static dataParaTexto(data) {
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+    }
+
+    static textoParaData(texto) {
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Formato da data inválido. Deve estar no formato aaaa-mm-dd.');
+
+        return new Date(...
+            texto.split('-')
+            .map((item, index) => item - index % 2)
+        );
     }
 }
