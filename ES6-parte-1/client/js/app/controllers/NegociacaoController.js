3951ea662dce988c8aa68ecb1000ac3319a9133b
joilson <joilson_rg@hotmail.com>
2020-09-28 08:42:27 -0300

    chore: passa a responsabilidade de atualizar tabela para a listaNegociacoes

--- ES6-parte-1/client/js/app/controllers/NegociacaoController.js
+++ ES6-parte-1/client/js/app/controllers/NegociacaoController.js
@@ -5,10 +5,11 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes((model) => {
+            this._negociacoesView.update(model);
+        });
         
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem = new Mensagem();
         this._mensagemView = new MensagemView($('#mensagemView'));
@@ -20,7 +21,6 @@ class NegociacaoController {
         event.preventDefault();
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         
         this._mensagem.texto = 'Negociação adicionada com sucesso.';
         this._mensagemView.update(this._mensagem);
@@ -30,7 +30,6 @@ class NegociacaoController {
 
     apaga() {
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociação apagadas com sucesso.';
         this._mensagemView.update(this._mensagem);
