f3f33419bc4e0915bfab4768e2f2afc652d2cdd1
joilson <joilson_rg@hotmail.com>
2020-09-28 11:23:11 -0300

    chore: refatoração

--- ES6-parte-1/client/js/app/controllers/NegociacaoController.js
+++ ES6-parte-1/client/js/app/controllers/NegociacaoController.js
@@ -6,24 +6,17 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = ProxyFactory.create(
+        this._listaNegociacoes = new Bind(
             new ListaNegociacoes(),
-            ['adiciona', 'esvazia'], 
-            model => this._negociacoesView.update(model)
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia'
         );
         
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        
-        this._negociacoesView.update(this._listaNegociacoes);
-
-        this._mensagem = ProxyFactory.create(
+        this._mensagem = new Bind(
             new Mensagem(),
-            ['texto'],
-            model => this._mensagemView.update(model)
+            new MensagemView($('#mensagemView')),
+            'texto'
         );
-
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
